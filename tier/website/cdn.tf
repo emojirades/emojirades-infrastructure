564138e91e6eb9b9fa78a4a3fa2c70e880ded76c
Michael Robbins <michael+github@dalmura.com.au>
2021-08-28 10:50:21 +1000

    Adding in DNS validation for the existing certificate, this already existed, just importing for clarity

--- tier/website/cdn.tf
+++ tier/website/cdn.tf
@@ -72,3 +72,27 @@ resource "aws_acm_certificate" "website_cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "website_cert_dns" {
+  for_each = {
+    for dvo in aws_acm_certificate.website_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.environment_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "website_cert_validation" {
+  provider = aws.us-east-1
+
+  certificate_arn         = aws_acm_certificate.website_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.website_cert_dns : record.fqdn]
+}
